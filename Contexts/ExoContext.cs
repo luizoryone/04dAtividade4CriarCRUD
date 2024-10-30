259504a2bb23a2c34108612d31f1f45ecadbae74
luiz oryone <luizoryone@gmail.com>
2024-10-30 14:46:40 -0300

    Add project files.

---
+++ Contexts/ExoContext.cs
@@ -0,0 +1,69 @@
+using Exo.WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Data.SqlClient;
+
+namespace Exo.WebApi.Contexts
+{
+    public class ExoContext : DbContext
+    {
+        public ExoContext()
+        {
+        }
+
+        public ExoContext(DbContextOptions<ExoContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                // String de conexão usando autenticação SQL Server.
+                optionsBuilder.UseSqlServer("Server=DESK_I5\\SQLEXPRESS; Database=ExoApi; User ID=sa; Password=1234; Trusted_Connection=False;");
+            }
+        }
+
+        public DbSet<Projeto> Projetos { get; set; }
+    }
+}
+
+
+
+
+
+
+
+
+//using Exo.WebApi.Models;
+//using Microsoft.EntityFrameworkCore;
+//using System.Data.SqlClient;
+//using Microsoft.Data.SqlClient;
+
+//namespace Exo.WebApi.Contexts
+//{
+//    public class ExoContext : DbContext
+//    {
+//        public ExoContext()
+//        {     
+//        }
+//        public ExoContext(DbContextOptions<ExoContext> options) : base(options)
+//        {
+//        }
+//        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//        {
+//            if(!optionsBuilder.IsConfigured)
+//            {
+//              // Essa string de conexão foi depende da SUA máquina.
+//                optionsBuilder.UseSqlServer("Server=\\DESK_I5\\SQLEXPRESS; Database=ExoApi;Trusted_Connection=True;");
+
+//              // Exemplo 1 de string de conexão:
+//              // User ID=sa;Password=admin;Server=localhost;Database=ExoApi;-
+//              // + Trusted_Connection=False;
+
+//              // Exemplo 2 de string de conexão:
+//              // Server=localhost\\SQLEXPRESS;Database=ExoApi;Trusted_Connection=True;
+//            }
+//        }
+//        public DbSet<Projeto> Projetos { get; set; }
+//    }
+//}
